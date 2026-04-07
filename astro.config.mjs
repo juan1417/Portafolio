e52cad359e15e8d36569730ea26f25f2ca6ae55a
CodingSpace <102328599+juan1417@users.noreply.github.com>
2026-04-06 20:31:14 -0500

    Remove output option from Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
   adapter: vercel(),
   vite: { plugins: [tailwindcss()] },
 });
